7b57a14df6eb4e6e4409ab28e61072fe80eef120
Latif Adams <latiftanga@gmail.com>
2021-11-24 21:57:58 +0000

    Added Postgresql Database service.

--- app/core/models.py
+++ app/core/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom User that support using email instead of username"""
     email = models.EmailField(max_length=64, unique=True)
     is_active = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
